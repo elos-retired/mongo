a1c26e9f4d306d973c4ba5db7852ef0907443c34
Nick Landolfi <nclandolfi@gmail.com>
2015-03-14 16:09:05 -0700

    add disclaimer to install_linux script

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "NOT TESTED"
 apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
 apt-get -y update
